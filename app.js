f7d29e38cb2e53bcda90b761bcbc5b920ea7a1bb
Tamir Izhak <tamir.izhak1987@gmail.com>
2020-03-14 18:10:34 +0200

    installed and required npm bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require ("md5");
+const bcrypt = require('bcrypt');
 
 const app = express();
 
