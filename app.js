53b53fc3ab500904954634580d678ca257c5f842
Tamir Izhak <tamir.izhak1987@gmail.com>
2020-03-19 22:44:00 +0200

    implemented facebook Oauth20 to app.js and added buttons on html

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 const app = express();
 
@@ -41,7 +42,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    facebookId: String
 });
 // add passportLocalMongoose plugin to userSchema (hashing and salting into mongodb)
 userSchema.plugin(passportLocalMongoose);
@@ -77,9 +79,33 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+//Facebook OAuth20 implementation
+passport.use(new FacebookStrategy({
+    clientID: process.env.APP_ID,
+    clientSecret: process.env.APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+       return cb(err, user);
+     });
+   }
+ ));
+
 app.get("/", function(req, res) {
     res.render("home");
 });
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get('/auth/google',
   passport.authenticate('google', { scope: ["profile"] })
@@ -91,8 +117,6 @@ app.get("/auth/google/secrets",
     // Successful authentication, redirect home.
     res.redirect("/secrets");
   });
-
-
 
 app.get("/login", function(req, res) {
 
